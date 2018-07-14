bd93acfb5686f0a5bf4f982405fccf71fbad050e
Ralf Westphal <info@ralfw.de>
2018-07-14 22:48:39 +0200

    colors are kept with each char / close #4

--- spike/spike/Program.cs
+++ spike/spike/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Linq;
+using System.Reflection;
 
 namespace spike
 {
@@ -28,7 +29,7 @@ namespace spike
                 Letter = 'O',
                 HasFocus = true,
                 TabIndex = 2,
-                Color = ConsoleColor.Yellow
+                Color = ConsoleColor.DarkMagenta
             });
             background.Children.Add(new Widget
             {
@@ -65,7 +66,86 @@ namespace spike
 
     class Renderer
     {
+        class Point
+        {
+            public char Symbol = '.';
+            public ConsoleColor Color = ConsoleColor.Black;
+        }
+
+        class Canvas
+        {
+            private readonly Point[,] _canvas;
+            
+            public Canvas(int height, int width) {
+                _canvas = new Point[height,width];
+            }
+
+            public Point this[int y, int x] {
+                get {
+                    if (y < 0 || y >= this.Height ||
+                        x < 0 || x >= this.Width) return null;
+                    return _canvas[y, x];
+                }
+                set {
+                    if (y < 0 || y >= this.Height ||
+                        x < 0 || x >= this.Width) return;
+                    _canvas[y, x] = value;
+                }
+            }
+
+            public int Width => _canvas.GetLength(1);
+            public int Height => _canvas.GetLength(0);
+        }
+        
         public static void Render(Widget root)
+        {
+            var canvas = new Canvas(Console.WindowHeight, Console.WindowWidth);
+            Render(canvas, root);
+            
+            // draw canvas
+            var t = Console.BackgroundColor;
+            for (var y = 0; y < canvas.Height; y++)
+                for (var x = 0; x < canvas.Width; x++) {
+                    var p = canvas[y, x];
+                    if (p == null) continue;
+                    Console.BackgroundColor = p.Color;
+                    Console.SetCursorPosition(x,y);
+                    Console.Write(p.Symbol);
+                }
+            Console.BackgroundColor = t;
+        }
+
+        static void Render(Canvas canvas, Widget widget)
+        {
+            for(var row=0; row < widget.H; row++) {
+                var y = widget.Y + row;
+                for (var col = 0; col < widget.W; col++) {
+                    var x = widget.X + col;
+                    canvas[y, x] = new Point {
+                        Symbol = widget.Letter,
+                        Color = widget.Color
+                    };
+                }
+            }
+
+            if (widget.HasFocus) {
+                var p = canvas[widget.Y, widget.X];
+                if (p != null) p.Symbol = '+';
+                p = canvas[widget.Y, widget.X + widget.W - 1];
+                if (p != null) p.Symbol = '+';
+                p = canvas[widget.Y + widget.H - 1, widget.X];
+                if (p != null) p.Symbol = '+';
+                p = canvas[widget.Y + widget.H - 1, widget.X + widget.W - 1];
+                if (p != null) p.Symbol = '+';
+            }
+            
+            foreach(var child in widget.Children)
+                Render(canvas, child);
+        }
+        
+        
+        
+        public static void Render1(Widget root)
         {
             var area = new char[root.H][];
             for (var y = 0; y < root.H; y++)
@@ -88,17 +168,35 @@ namespace spike
                 area[root.H - 1][root.W - 1] = '+';
             }
 
+            
+            var displayHeight = Math.Min(root.H, Console.WindowHeight);
+            if (root.Y >= displayHeight)
+            { 
+                Console.WriteLine($"{root.Y}>={displayHeight}, <{root.Letter}>");
+                return;
+            }
+            var viewportWidth = Math.Min(root.X + root.W - 1, Console.WindowWidth);
+            if (root.X >= viewportWidth) return;
+
             var c = Console.BackgroundColor;
             Console.BackgroundColor = root.Color;
-            for (var y = 0; y < root.H; y++)
-            {
-                Console.SetCursorPosition(root.X, root.Y + y);
-                Console.Write(area[y]);
+            for (var y = 0; y < displayHeight; y++) {
+                try
+                {
+                    Console.SetCursorPosition(root.X, root.Y + y);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"({root.X},{root.Y+y} - {Console.WindowWidth}/{Console.WindowHeight}");
+                    Environment.Exit(1);
+                }
+
+                Console.Write(new String(area[y]).Substring(0, viewportWidth));
             }
             Console.BackgroundColor = c;
             
             foreach(var child in root.Children)
-                Render(child);
+                Render1(child);
         }
     }
 
