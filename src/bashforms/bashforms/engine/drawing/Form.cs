af3738de2c482cc9f6c5db9eefa246e299c472b2
Ralf Westphal <info@ralfw.de>
2018-07-18 09:59:49 +0200

    e improved form border drawing

--- src/bashforms/bashforms/engine/drawing/Form.cs
+++ src/bashforms/bashforms/engine/drawing/Form.cs
@@ -15,19 +15,19 @@ namespace bashforms.engine.drawing
 
             void RenderFrame() {
                 for (var y = 0; y < canvas.Height; y++) {
-                    canvas[0, y].Symbol = '|';
-                    canvas[canvas.Width - 1, y].Symbol = '|';
+                    canvas[0, y].Symbol = '│';
+                    canvas[canvas.Width - 1, y].Symbol = '│';
                 }
 
                 for (var x = 0; x < canvas.Width; x++){
-                    canvas[x, 0].Symbol = '–';
-                    canvas[x, canvas.Height - 1].Symbol = '-';
+                    canvas[x, 0].Symbol = '─';
+                    canvas[x, canvas.Height - 1].Symbol = '─';
                 }
             
-                canvas[0, 0].Symbol = '+';
-                canvas[canvas.Width-1, 0].Symbol = '+';
-                canvas[0, canvas.Height-1].Symbol = '+';
-                canvas[canvas.Width-1, canvas.Height-1].Symbol = '+';
+                canvas[0, 0].Symbol = '┌';
+                canvas[canvas.Width-1, 0].Symbol = '┐';
+                canvas[0, canvas.Height-1].Symbol = '└';
+                canvas[canvas.Width-1, canvas.Height-1].Symbol = '┘';
             }
 
             void RenderTitle() {
